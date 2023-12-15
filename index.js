dac8d68356f0896d537590a5aeca4e79b36d8c6f
jomin07 <107167492+jomin07@users.noreply.github.com>
2023-12-15 16:05:14 +0530

    no cache

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ dotenv.config();
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO);
 const path = require('path');
+const nocache = require('nocache');
 
 const express = require("express");
 const app = express();
+
+app.use(nocache());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
