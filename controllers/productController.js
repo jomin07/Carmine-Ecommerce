76427a40c1b31d6c3a12fd50cf6cdc286048d3d2
jomin07 <107167492+jomin07@users.noreply.github.com>
2023-12-18 00:25:13 +0530

    Product Image Delete

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const Product = require('../models/productModel');
 const Category = require('../models/categoryModel');
 
@@ -5,6 +7,7 @@ const loadProducts = async(req,res) =>{
     try {
         const productsData = await Product.find({}).populate('category');
         res.render('products',{products: productsData});
+    
     } catch (error) {
         console.log(error.message);
     }
@@ -14,6 +17,7 @@ const loadAddProduct = async(req,res) =>{
     try {
         const categoriesData = await Category.find({status: true});
         res.render('add-product',{categories: categoriesData});
+    
     } catch (error) {
         console.log(error.message);
     }
@@ -54,8 +58,81 @@ const addProduct = async(req,res) =>{
     }
 }
 
+const editProduct = async(req,res) =>{
+    try {
+        const id = req.query.id;
+        const productData = await Product.findById({_id: id}).populate('category');
+        const categoriesData = await Category.find({status: true});
+
+        if (productData) {
+            res.render('edit-product',{product: productData,categories: categoriesData});
+        } else {
+            res.redirect('/admin/products');
+        }
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const deleteImage = async (req, res) => {
+    try {
+        const id = req.query.id
+        const image = req.query.imageId
+        await Product.updateOne({ _id : id },{ $pull: {image: image}});
+        fs.unlink( path.join( __dirname, '../public/admin/productImages/') + image , ( err ) => {
+            if( err ) {
+                console.log(err);
+            }
+        });
+        res.redirect('/admin/products/');
+    } catch ( error ) {
+        console.log(error.message);
+
+    }
+}
+
+const updateProduct = async(req,res) =>{
+    try {
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const listProduct = async(req,res) =>{
+    try {
+        const id = req.query.id;
+        const productData = await Product.findById(id);
+        await productData.updateOne({$set: {status: true}});
+
+        res.redirect('/admin/products');
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const unListProduct = async(req,res) =>{
+    try {
+        const id = req.query.id;
+        const productData = await Product.findById(id);
+        await productData.updateOne({$set: {status: false}});
+        
+        res.redirect('/admin/products');
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
 module.exports = {
     loadProducts,
     loadAddProduct,
-    addProduct
+    addProduct,
+    editProduct,
+    deleteImage,
+    updateProduct,
+    listProduct,
+    unListProduct
 }
